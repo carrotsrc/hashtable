3df447cc77484ac2f1a4f089ef9e7a7ea7e5a59d
Charlie FG <charlie@carrotsrc.org>
2014-07-11 13:38:14 +0100

    Updated notice

--- hash_table.h
+++ hash_table.h
@@ -1,5 +1,5 @@
 /*
-* Copyright 2014, carrotsrc.org
+* Copyright 2014, Charlie Fyvie-Gauld (Carrotsrc.org)
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
